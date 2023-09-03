65be91ce87eede3476b8c567c05dce9d6d39b2c9
aahanatiwari <aahanatiwari18@gmail.com>
2023-09-03 16:43:14 +0530

    responsiveness added

--- Sponsors-Page/sponsors-script.js
+++ Sponsors-Page/sponsors-script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.sponsors img').forEach(image =>{
+document.querySelectorAll('.sponsor-logo img:nth-child(2)').forEach(image =>{
     image.onclick = () =>{
         document.querySelector('.popup-image').style.display = 'block';
         document.querySelector('.popup-image img').src = image.getAttribute('src');
